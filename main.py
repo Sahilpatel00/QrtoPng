7651e937b7232b056eb51b2d0fbd40c5f1da8aaa
Sahil Godhani <sgodhani411@rku.ac.in>
2024-03-30 18:18:00 +0530

    get request

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ app = FastAPI()
 @app.get("/generate_qr_code/")
 async def generate_qr_code(json_data: str = Query(None), size: int = Query(None)):
     try:
-        # json_data = data.get("json_data")
-        # size = data.get("size")
+
 
         if not json_data or not size:
             raise HTTPException(status_code=400, detail="JSON data and size are required")
