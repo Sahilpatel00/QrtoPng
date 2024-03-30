0707fcfd4a334432a14513d14532490c2d6ef8f2
Sahil Godhani <52962238+Sahilpatel00@users.noreply.github.com>
2024-03-30 15:34:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from fastapi import FastAPI, HTTPException, Response
+from fastapi import FastAPI, HTTPException, Response, Query
 import qrcode
 from io import BytesIO
 from pydantic import BaseModel
 app = FastAPI()
 
-@app.post("/generate_qr_code/")
-async def generate_qr_code(data: dict):
+@app.get("/generate_qr_code/")
+async def generate_qr_code(json_data: str = Query(None), size: int = Query(None)):
     try:
-        json_data = data.get("json_data")
-        size = data.get("size")
+        # json_data = data.get("json_data")
+        # size = data.get("size")
 
         if not json_data or not size:
             raise HTTPException(status_code=400, detail="JSON data and size are required")
