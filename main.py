1a99be8af5cdf9319b729518fcac7016548ee267
Sahil Godhani <sgodhani411@rku.ac.in>
2024-04-01 00:30:29 +0530

    get request

--- main.py
+++ main.py
@@ -2,14 +2,25 @@ from fastapi import FastAPI, HTTPException, Response, Query
 import qrcode
 from io import BytesIO
 from PIL import Image
+import urllib.parse
 
 app = FastAPI()
 
 @app.get("/generate_qr_code/")
-async def generate_qr_code(json_data: str = Query(None), size: int = Query(None), image_path: str = Query(None)):
+async def generate_qr_code(
+    json_data: str = Query(None),
+    size: int = Query(None),
+    image_path: str = Query(None)
+):
     try:
         if not json_data or not size or not image_path:
             raise HTTPException(status_code=400, detail="JSON data, size, and image path are required")
+
+        # URL-decode the file path
+        image_path = urllib.parse.unquote(image_path)
+
+        # Load the image from the provided file path
+        img = Image.open(image_path)
 
         # Create a QR code instance
         qr = qrcode.QRCode(
@@ -22,30 +33,26 @@ async def generate_qr_code(json_data: str = Query(None), size: int = Query(None)
         qr.make(fit=True)
 
         # Create an image from the QR Code instance
-        img = qr.make_image(fill_color="black", back_color="white").convert('RGB')
+        qr_img = qr.make_image(fill_color="black", back_color="white").convert('RGB')
 
         # Resize the QR code
-        img = img.resize((size, size))
+        qr_img = qr_img.resize((size, size))
 
-        # Load the image to be inserted into the QR code
-        logo = Image.open('C:/Users/sahil/Desktop/QrtoPng-2/LOGO FINAL IBS Var 2.jpg')
-
-        # Calculate the size of the image to be inserted
+        # Calculate the size of the uploaded image to be inserted into the QR code
         logo_size = int(size / 5)  # The logo will take up 1/5th of the QR code size
 
-        # Resize the logo as per the calculated size
-        logo = logo.resize((100, 100))
-        
+        # Resize the uploaded image as per the calculated size
+        img = img.resize((100, 100))
 
         # Calculate the position where the image should be inserted
-        pos = ((img.size[0] - logo.size[0]) // 2, (img.size[1] - logo.size[1]) // 2)
+        pos = ((qr_img.size[0] - img.size[0]) // 2, (qr_img.size[1] - img.size[1]) // 2)
 
         # Insert the image in the center of the QR code
-        img.paste(logo, pos)
+        qr_img.paste(img, pos)
 
         # Save the QR code with the image to a buffer
         img_buffer = BytesIO()
-        img.save(img_buffer, format="PNG")
+        qr_img.save(img_buffer, format="PNG")
         img_buffer.seek(0)
 
         # Get the bytes value of the image
